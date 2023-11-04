e60eeaabf643aba5ea5c61d1b2a761194f40835d
TamannaAfroz2666 <afroztamanna66@gmail.com>
2023-11-04 15:32:00 +0600

    basic header created and styled

--- src/layout/Header/Header.js
+++ src/layout/Header/Header.js
@@ -8,72 +8,76 @@ const Header = () => {
     return (
         <div className='headerContainer'>
             <div className="containerHead">
-                <div className="containerBody">
-                    <div className="headerLogo">
-                        <Link to='/home'>
-                        <p className='logoTitle'>TA</p>
-                        <span className='logoSub'>shop</span>
-                        </Link>
-                    </div>
-                    <div className="navLinks">
-                        <ul className='linksHead'>
-                            <li >
-                                <Link className='linksTitle'>shop all</Link>
-                            </li>
-                            <li>
-                                <Link className='linksTitle'>Decor</Link>
-                            </li>
-                            <li>
-                                <Link className='linksTitle'>office</Link>
-                            </li>
-                            <li>
-                                <Link className='linksTitle'>living room</Link>
-                            </li>
-                            <li>
-                                <Link className='linksTitle'>bedroom</Link>
-                            </li>
-                            <li>
-                                <Link className='linksTitle'>
-                                    <button>
-                                        <AiOutlineSearch size={20} color='black' />
-                                    </button>
-                                </Link>
-                            </li>
-                        </ul>
-
-                    </div>
-                    <div className="subLinks">
-                        <ul>
-                            <li>
-                                <Link>Story</Link>
-                            </li>
-                            <li>
-                                <Link>contact</Link>
-                            </li>
-                            <li>
-                                <Link>track order</Link>
-                            </li>
-                            <li>
-                                <Link>help</Link>
-                            </li>
-                        </ul>
-                        <div className="subSubLinks">
-                            <div className="logSub">
-                                <Link>Log In</Link>
-                            </div>
-                            <div className="cartSubLink">
-                                <Link>
-                                    <BsBasket color='tomato' size={30} />
-                                </Link>
-
-                            </div>
-
+                <div className="headerHight">
+                    <div className="containerBody">
+                        <div className="headerLogo">
+                            <Link to='/home'>
+                                <p className='logoTitle'>TA</p>
+                                <span className='logoSub'>shop</span>
+                            </Link>
+                        </div>
+                        <div className="navLinks">
+                            <ul className='linksHead'>
+                                <li >
+                                    <Link className='linksTitle'>shop all</Link>
+                                </li>
+                                <li>
+                                    <Link className='linksTitle'>Decor</Link>
+                                </li>
+                                <li>
+                                    <Link className='linksTitle'>office</Link>
+                                </li>
+                                <li>
+                                    <Link className='linksTitle'>living room</Link>
+                                </li>
+                                <li>
+                                    <Link className='linksTitle'>bedroom</Link>
+                                </li>
+                                <li>
+                                    <Link className='linksTitle'>
+                                        <button>
+                                            <AiOutlineSearch size={20} color='black' />
+                                        </button>
+                                    </Link>
+                                </li>
+                            </ul>
 
                         </div>
+                        <div className="subLinks">
+                            <ul className='subLinkHead'>
+                                <li>
+                                    <Link className='subLinksTitle'>Story</Link>
+                                </li>
+                                <li>
+                                    <Link className='subLinksTitle'>contact</Link>
+                                </li>
+                                <li>
+                                    <Link className='subLinksTitle'>track order</Link>
+                                </li>
+                                <li>
+                                    <Link className='subLinksTitle'>help</Link>
+                                </li>
+                            </ul>
+                            <div className="subSubLinks">
+                                <div className="logSub">
+                                    <Link className='loginHeadTitle'>Log In</Link>
+                                </div>
+                                <div className="cartSubLink">
+                                    <Link>
+                                        <BsBasket color='tomato' size={20} />
+                                    </Link>
+
+                                </div>
+
+
+                            </div>
+                        </div>
+
+
                     </div>
 
-
                 </div>
+
             </div>
         </div>
     );
