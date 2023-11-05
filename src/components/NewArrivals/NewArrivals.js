f6b22570db00e66966ee03f3e29be30ff6aff297
TamannaAfroz2666 <afroztamanna66@gmail.com>
2023-11-05 22:29:49 +0600

    new arrivals page created and well designed

--- src/components/NewArrivals/NewArrivals.js
+++ src/components/NewArrivals/NewArrivals.js
@@ -4,13 +4,13 @@ import { Link } from 'react-router-dom';
 import { motion } from 'framer-motion';
 
 const NewArrivals = () => {
-    const dressHandle =() =>{
+    const dressHandle = () => {
         console.log('dress are here');
     }
-    const accessHanle = () =>{
+    const accessHanle = () => {
         console.log('all needs are ready');
     }
-    const shoeHandle = () =>{
+    const shoeHandle = () => {
         console.log('shoes here');
     }
     return (
@@ -18,36 +18,50 @@ const NewArrivals = () => {
             <div className="arrivalsContent">
                 <div className="multiPro">
                     <div className="dressContent">
+                    <Link to='dress-store'>
                         <motion.button
-                           whileHover={{ 
-                          scale: 1.1
-                           }}
-                           
-                        className='dressBtn'
-                        onClick={dressHandle} 
+                            whileHover={{
+                                scale: 1.1
+                            }}
+
+                            className='dressBtn'
+                            onClick={dressHandle}
                         >Dress</motion.button>
+                        </Link>
                     </div>
                     <div className="multiCon">
-                        <div className="parse">
-                            <motion.button
-                            className='accessBtn'
-                            whileHover={{scale:1.1}}
-                            onClick={accessHanle}
-                            >
-                                Accessories
-                                </motion.button>
+                       
+                            <div className="parse">
+                                <Link to='accessories-store'>
+                                    <motion.button
+                                        className='accessBtn'
+                                        whileHover={{ scale: 1.1 }}
+                                        onClick={accessHanle}
+                                    >
+                                        Accessories
+                                    </motion.button>
+                                </Link>
 
-                        </div>
+                            </div>
+                        
+
+
+
+
                         <div className="shoes">
+                        <Link to='shoes-store'>
                             <motion.button
-                            className='shoesBtn'
-                            onCanPlay={shoeHandle}
-                            whileHover={{scale: 1.1}}
+                                className='shoesBtn'
+                                onCanPlay={shoeHandle}
+                                whileHover={{ scale: 1.1 }}
                             >
                                 Shoes
                             </motion.button>
+                            </Link>
+
 
                         </div>
+
                     </div>
 
                 </div>
@@ -56,13 +70,16 @@ const NewArrivals = () => {
                         <h5 className='arrivalsTitle'>new arrivals</h5>
                         <h3 className='blondesTitle'>blondes <br /> with minimalist</h3>
                         <div className="shopColl">
-                            <Link>Shop Collection</Link>
+                            <Link to='/shop-collection' className='shopCollection'>Shop Collection
+
+                                <span className='hh'><hr className='shopCollectionHr' /></span>
+                            </Link>
                         </div>
                     </div>
 
                 </div>
             </div>
-            
+
         </div>
     );
 };
