29814c8869cd2e12f92fed8278cbc3e3c8738f97
TamannaAfroz2666 <afroztamanna66@gmail.com>
2023-11-05 18:46:37 +0600

    add motion on side

--- src/components/BackgroundImage/BackgroundImage.js
+++ src/components/BackgroundImage/BackgroundImage.js
@@ -1,25 +1,31 @@
 import React from 'react';
 import './BackgroundImage.css';
-import image1 from '../../assets/Image/sandle.jpg';
-// import img2 from '../../assets/Image/bg4.jpg';
-// import img3 from '../../assets/Image/bg3.jpg'
+import { motion } from 'framer-motion';
+
+
 
 
 const BackgroundImage = () => {
 
-    const shopNow =() =>{
+    const shopNow = () => {
         console.log('shop now available');
     }
     return (
         <div className='backgroundImage'>
             <div className="backHead">
-                <div className="backContents">
+                <motion.div
+                  initial={{ opacity: 0, x: '-70vh' }}
+                  animate={{ opacity: 1, x: 0 }}
+                  transition={{ duration: 1 }}
+
+                    className="backContents"
+                >
                     <h5 className='blackFri'>Black Friday on July</h5>
                     <h2 className='uptoTitle'>Up to 50% off</h2>
                     <h3 className='hundredTitle'>Hundreds of styles available</h3>
                     <button
-                    className='shopBtn'
-                    onClick={shopNow}
+                        className='shopBtn'
+                        onClick={shopNow}
 
                     >
                         Shop Now
@@ -27,7 +33,7 @@ const BackgroundImage = () => {
                     <div className="imageBack">
 
                     </div>
-                </div>
+                </motion.div>
 
             </div>
 
