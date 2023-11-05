b241da4a792addbd8688452f76bb42c384d8e7c7
TamannaAfroz2666 <afroztamanna66@gmail.com>
2023-11-05 19:26:37 +0600

    base create on this page

--- src/components/NewArrivals/NewArrivals.js
+++ src/components/NewArrivals/NewArrivals.js
@@ -1,9 +1,38 @@
 import React from 'react';
 import './NewArrivals.css';
+import { Link } from 'react-router-dom';
 
 const NewArrivals = () => {
     return (
         <div className='newArrivals'>
+            <div className="arrivalsContent">
+                <div className="multiPro">
+                    <div className="dressContent">
+                        <button>Dress</button>
+                    </div>
+                    <div className="multiCon">
+                        <div className="parse">
+                            <button>Accesories</button>
+
+                        </div>
+                        <div className="shoes">
+                            <button>Shoes</button>
+
+                        </div>
+                    </div>
+
+                </div>
+                <div className="singlePro">
+                    <div className="singleImg">
+                        <h5 className='arrivalsTitle'>new arrivals</h5>
+                        <h3>blondes with minimalist</h3>
+                        <div className="shopColl">
+                            <Link>Shop Collection</Link>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
             
         </div>
     );
--- src/layout/Main/Main.js
+++ src/layout/Main/Main.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './Main.css'
 import Header from '../Header/Header';
 import BackgroundImage from '../../components/BackgroundImage/BackgroundImage';
+import NewArrivals from '../../components/NewArrivals/NewArrivals';
 
 const Main = () => {
     return (
         <div className='main'>
             <Header></Header>
             <BackgroundImage></BackgroundImage>
+            <NewArrivals></NewArrivals>
             
         </div>
     );
