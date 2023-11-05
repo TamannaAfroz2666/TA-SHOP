1d61ef373707407ab810b0d07d63fd73fe47b2f3
TamannaAfroz2666 <afroztamanna66@gmail.com>
2023-11-05 11:07:35 +0600

    set the bootstrap

--- src/components/BackgroundImage/BackgroundImage.js
+++ src/components/BackgroundImage/BackgroundImage.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import './BackgroundImage.css';
+import Button from 'react-bootstrap/Button';
 
 const BackgroundImage = () => {
     return (
         <div className='backgroundImage'>
+            <h1>gg</h1>
+            <Button variant="primary">Primary</Button>{' '}
             
         </div>
     );
--- src/layout/Main/Main.js
+++ src/layout/Main/Main.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './Main.css'
 import Header from '../Header/Header';
+import BackgroundImage from '../../components/BackgroundImage/BackgroundImage';
 
 const Main = () => {
     return (
         <div className='main'>
             <Header></Header>
+            <BackgroundImage></BackgroundImage>
             
         </div>
     );
