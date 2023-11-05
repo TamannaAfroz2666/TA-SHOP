1f6cc3ceff3c645d967929a0803750d57e151d3a
TamannaAfroz2666 <afroztamanna66@gmail.com>
2023-11-05 18:50:36 +0600

    working on new arrivals section

---
+++ src/components/NewArrivals/NewArrivals.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './NewArrivals.css';
+
+const NewArrivals = () => {
+    return (
+        <div className='newArrivals'>
+            
+        </div>
+    );
+};
+
+export default NewArrivals;
