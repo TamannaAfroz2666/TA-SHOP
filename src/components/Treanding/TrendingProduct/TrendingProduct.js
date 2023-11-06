7c2804c21aa9eb1f7efec638607bea04ee9fe9da
TamannaAfroz2666 <afroztamanna66@gmail.com>
2023-11-06 13:49:55 +0600

    animate add

--- src/components/Treanding/TrendingProduct/TrendingProduct.js
+++ src/components/Treanding/TrendingProduct/TrendingProduct.js
@@ -1,26 +1,55 @@
 import React from 'react';
 import './TrendingProduct.css';
 import img1 from '../../../assets/Image/NewArrivals/pic20.jpg';
-import { AiOutlineHeart , AiOutlineEye } from "react-icons/ai";
+import { AiOutlineHeart, AiOutlineEye } from "react-icons/ai";
 import { HiArrowsUpDown } from "react-icons/hi2";
+import { motion } from 'framer-motion';
 
 const TrendingProduct = () => {
     return (
         <div className='trendingProduct'>
             <div className="productContainer">
                 <div className="productContents">
-                    <img src={img1} alt="" className='image'/>
+                    <img src={img1} alt="" className='image' />
                     <div className="overlaySection">
                         <div className="iconSection">
-                            <div className="heart">
-                                <AiOutlineHeart className='IconOverlay' color='black' size={35}/>
-                            </div>
-                            <div className="arrow">
-                                <HiArrowsUpDown  className='IconOverlay' size={35}/>
-                            </div>
-                            <div className="eye">
-                                <AiOutlineEye  className='IconOverlay' size={35}/>
-                            </div>
+                            <motion.div
+                                whileHover={{
+                                    scale: 1.2,
+                                    opacity: 1, x: '-.1vh'
+
+                                }}
+
+                                animate={{ opacity: 1, x: 0 }}
+                                transition={{ duration: .2 }}
+                                className="heart"
+                            >
+                                <AiOutlineHeart className='IconOverlay' color='black' size={35} />
+                            </motion.div>
+                            <motion.div
+                                whileHover={{
+                                    scale: 1.2,
+                                    opacity: 1, x: '-.1vh'
+
+                                }}
+
+                                animate={{ opacity: 1, x: 0 }}
+                                transition={{ duration: .2 }}
+
+                                className="arrow">
+                                <HiArrowsUpDown className='IconOverlay' size={35} />
+                            </motion.div>
+                            <motion.div
+                                whileHover={{
+                                    scale: 1.2,
+                                    opacity: 1, x: '-.1vh'
+                                }}
+
+                                animate={{ opacity: 1, x: 0 }}
+                                transition={{ duration: .2 }}
+                                className="eye">
+                                <AiOutlineEye className='IconOverlay' size={35} />
+                            </motion.div>
 
                         </div>
                         <h1>gg</h1>
@@ -31,7 +60,7 @@ const TrendingProduct = () => {
 
             </div>
             .
-            
+
         </div>
     );
 };
