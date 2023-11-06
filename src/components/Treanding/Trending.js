7e453cd3c29359546af9ef2ebe50c38aa65689fb
TamannaAfroz2666 <afroztamanna66@gmail.com>
2023-11-06 12:33:34 +0600

    show more link here

--- src/components/Treanding/Trending.js
+++ src/components/Treanding/Trending.js
@@ -8,24 +8,29 @@ const Trending = () => {
     return (
         <div className='trending'>
             <div className="trendingBody">
-                <div className="abc">
-                    <h1 className='trendTitle'>You are in   </h1>
-                </div>
+                <div className="infoContent">
+                    <div className="abc">
+                        <h1 className='trendTitle'>You are in</h1>
+                    </div>
 
-                <div className="nav_link">
-                    <div className="dropdown">
-                        <span className="dropbtn">trending <BsArrowDown size={25} className='trendingHr' /> </span>
-                        <div className="dropdown-content">
-                            <Link to="/trending-sell">trending</Link>
-                            <Link to="/best-seller">best seller</Link>
-                            <Link to="/top-ranking">top ranking</Link>
+                    <div className="nav_link">
+                        <div className="dropdown">
+                            <span className="dropbtn">trending <BsArrowDown size={25} className='trendingHr' /> </span>
+                            <div className="dropdown-content">
+                                <Link to="/trending-sell">trending</Link>
+                                <Link to="/best-seller">best seller</Link>
+                                <Link to="/top-ranking">top ranking</Link>
+                            </div>
                         </div>
+
                     </div>
 
                 </div>
-
-
-
+                <div className="showMorePro">
+                    <Link to="show-all-product" className='showAllTitle'>Show all product
+                    <span className='hh'><hr className='showAllHr' /></span>
+                    </Link>
+                </div>
 
             </div>
             <h1 className='hhhh'>hhh</h1>
