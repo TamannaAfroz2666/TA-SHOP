1a39ae07aab067076e0a85c869d769c0f2b70e7d
TamannaAfroz2666 <afroztamanna66@gmail.com>
2023-11-06 13:28:30 +0600

    icon designed

--- src/components/Treanding/TrendingProduct/TrendingProduct.js
+++ src/components/Treanding/TrendingProduct/TrendingProduct.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import './TrendingProduct.css';
-import img1 from '../../../assets/Image/NewArrivals/pic20.jpg'
+import img1 from '../../../assets/Image/NewArrivals/pic20.jpg';
+import { AiOutlineHeart , AiOutlineEye } from "react-icons/ai";
+import { HiArrowsUpDown } from "react-icons/hi2";
 
 const TrendingProduct = () => {
     return (
@@ -9,6 +11,18 @@ const TrendingProduct = () => {
                 <div className="productContents">
                     <img src={img1} alt="" className='image'/>
                     <div className="overlaySection">
+                        <div className="iconSection">
+                            <div className="heart">
+                                <AiOutlineHeart className='IconOverlay' color='black' size={35}/>
+                            </div>
+                            <div className="arrow">
+                                <HiArrowsUpDown  className='IconOverlay' size={35}/>
+                            </div>
+                            <div className="eye">
+                                <AiOutlineEye  className='IconOverlay' size={35}/>
+                            </div>
+
+                        </div>
                         <h1>gg</h1>
                         <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
 
