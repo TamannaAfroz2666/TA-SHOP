e0837b99110c961e11228272a54e19dcb8e1fc3e
TamannaAfroz2666 <afroztamanna66@gmail.com>
2023-11-06 12:39:58 +0600

    working on product page

---
+++ src/components/Treanding/TrendingProduct/TrendingProduct.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './TrendingProduct.css';
+
+const TrendingProduct = () => {
+    return (
+        <div className='trendingProduct'>
+            
+        </div>
+    );
+};
+
+export default TrendingProduct;
