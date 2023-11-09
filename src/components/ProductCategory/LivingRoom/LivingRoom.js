2926a6e93c0153f3897876a844bd2a9e49034688
TamannaAfroz2666 <afroztamanna66@gmail.com>
2023-11-09 11:51:18 +0600

    added new page

--- src/components/ProductCategory/LivingRoom/LivingRoom.js
+++ src/components/ProductCategory/LivingRoom/LivingRoom.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import './LivingRoom.css';
+import img from '../../../assets/Image/category/livingRoom.jpg';
 
 const LivingRoom = () => {
     return (
-        <div>
-            
+        <div className='livingRoomHead'>
+            <a href="product-category/living-room/">
+                <div className="livingSub">
+                    <img src={img} alt="" />
+                    <div className="livingInfo">
+                        <p className='title_living'>bedroom</p>
+                        <p className='count_living'>6 products</p>
+                    </div>
+                </div>
+            </a>
+
         </div>
     );
 };
