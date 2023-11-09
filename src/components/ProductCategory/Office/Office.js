223d32fbb1b707935a3aa4d292d926a35e6e6a6c
TamannaAfroz2666 <afroztamanna66@gmail.com>
2023-11-09 11:47:27 +0600

    added new page

--- src/components/ProductCategory/Office/Office.js
+++ src/components/ProductCategory/Office/Office.js
@@ -1,8 +1,21 @@
 import React from 'react';
+import './Office.css';
+import img from '../../../assets/Image/category/office.jpg';
 
 const Office = () => {
     return (
-        <div>
+        <div className='office_main'>
+            <a href="/product-category/office/">
+                <div className="officeSub">
+                    <img src={img} alt="" />
+                    <div className="office_info">
+                    <p className='title_office'>bedroom</p>
+                        <p className='count_office'>6 products</p>
+                    </div>
+                </div>
+
+
+            </a>
             
         </div>
     );
