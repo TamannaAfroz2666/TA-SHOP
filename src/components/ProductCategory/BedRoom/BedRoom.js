794ec19e0d229e18accf149326c17a9e7078b645
TamannaAfroz2666 <afroztamanna66@gmail.com>
2023-11-09 11:37:53 +0600

    page create

--- src/components/ProductCategory/BedRoom/BedRoom.js
+++ src/components/ProductCategory/BedRoom/BedRoom.js
@@ -5,15 +5,17 @@ import image from '../../../assets/Image/category/bedroom.jpg'
 const BedRoom = () => {
     return (
         <div className='bed_room'>
-            <div className="sub_bedRoom">
-                <img src={image} alt="" />
-                <div className="info_bed">
-                    <p className='title_bed'>bedroom</p>
-                    <p className='count_bed'>6 products</p>
+            <a href="product-category/bedroom/">
+                <div className="sub_bedRoom">
+                    <img src={image} alt="" />
+                    <div className="info_bed">
+                        <p className='title_bed'>bedroom</p>
+                        <p className='count_bed'>6 products</p>
+                    </div>
                 </div>
-            </div>
-            
-            
+            </a>
+
+
         </div>
     );
 };
