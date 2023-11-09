e7a826e1a2587d8b43005852f0539fe292a14a88
TamannaAfroz2666 <afroztamanna66@gmail.com>
2023-11-09 16:06:22 +0600

    spring page complete

--- src/components/SpringCollection/SpringCollection.js
+++ src/components/SpringCollection/SpringCollection.js
@@ -2,9 +2,8 @@ import React from 'react';
 import './SpringCollection.css';
 import img from '../../assets/Image/SpringCollection/look1.webp'
 import im2 from '../../assets/Image/SpringCollection/1.avif';
-import im3 from '../../assets/Image/SpringCollection/look2.webp';
 import im4 from '../../assets/Image/SpringCollection/look3.webp';
-import { motion } from 'framer-motion';
+
 
 
 
@@ -12,16 +11,24 @@ const SpringCollection = () => {
     return (
         <div className='springCollection'>
             <div className="springCollSub">
-                <div className="cateTitleContent">
-                    <h5 className='shopByCate'>New Collection</h5>
-                    <h1 className='shopByCateMain'>The Spring Collection</h1>
-                    <hr className='cateHr' />
+                <div className="springContents">
+                    <div className="cateTitleContent">
+                        <h5 className='shopByCate'>New Collection</h5>
+                        <h1 className='shopByCateMain'>The Spring Collection</h1>
+                        <hr className='cateHr' />
+                    </div>
+                    <div className="imgSpring">
+                        <a href="/spring-girls">
+                            <img src={img} alt="" className='imge1' />
+                        </a>
+                        <a href="/spring-new-collection">
+                            <img src={im2} alt="" className='fff1' /></a>
+                        <a href="/spring-westerns">
+                            <img src={im4} alt="" className='fff2' /></a>
+                    </div>
+
                 </div>
-                <div className="imgSpring">
-                    <img src={img} alt="" className='imge1' />
-                    <motion.img src={im2} alt="" className='fff1' />
-                    <img src={im4} alt="" className='fff2' />
-                </div>
+
 
 
             </div>
