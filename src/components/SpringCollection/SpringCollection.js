b0c0c2988a78b5326253c061aa1c750b9b34d7af
TamannaAfroz2666 <afroztamanna66@gmail.com>
2023-11-09 15:52:40 +0600

    title add

--- src/components/SpringCollection/SpringCollection.js
+++ src/components/SpringCollection/SpringCollection.js
@@ -12,22 +12,20 @@ const SpringCollection = () => {
     return (
         <div className='springCollection'>
             <div className="springCollSub">
+                <div className="cateTitleContent">
+                    <h5 className='shopByCate'>New Collection</h5>
+                    <h1 className='shopByCateMain'>The Spring Collection</h1>
+                    <hr className='cateHr' />
+                </div>
                 <div className="imgSpring">
-                <img src={img} alt="" className='imge1'/>
-                <div className="img2">
-                <motion.img
-               
-                 src={im2} alt="" className='fff1' />
+                    <img src={img} alt="" className='imge1' />
+                    <motion.img src={im2} alt="" className='fff1' />
+                    <img src={im4} alt="" className='fff2' />
                 </div>
-             
-                <img src={im4} alt="" className='fff2'/>
-             
 
-                </div>
-               
 
             </div>
-            
+
         </div>
     );
 };
