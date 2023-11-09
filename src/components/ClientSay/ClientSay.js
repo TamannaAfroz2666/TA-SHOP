7f909ac8546ac51b1efa4d34735f1a4b9b552444
TamannaAfroz2666 <afroztamanna66@gmail.com>
2023-11-09 16:46:30 +0600

    base create

--- src/components/ClientSay/ClientSay.js
+++ src/components/ClientSay/ClientSay.js
@@ -1,5 +1,9 @@
-import React, { useRef, useState } from 'react';
+import React from 'react';
 import './ClientSay.css';
+import img1 from '../../assets/Image/Clients/Pic1.jpg'
+import img2 from '../../assets/Image/Clients/pic2.jpg'
+import img3 from '../../assets/Image/Clients/pic3.jpg'
+import img4 from '../../assets/Image/Clients/pic4.jpg'
 
 const ClientSay = () => {
 
@@ -8,6 +12,21 @@ const ClientSay = () => {
             <div className="clientSub">
                 <div className="clientTitleContent">
                     <h5 className='ClientTitle'>from the people</h5>
+                </div>
+                <div className="clientContents">
+                    <div className="block1">
+
+                    </div>
+                    <div className="block2">
+
+                    </div>
+                    <div className="block3">
+
+                    </div>
+                    <div className="block4">
+
+                    </div>
+
                 </div>
 
 
