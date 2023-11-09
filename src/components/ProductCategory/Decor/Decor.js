03772c9bfb6500f262b1d39229661452dd3411d1
TamannaAfroz2666 <afroztamanna66@gmail.com>
2023-11-09 11:55:16 +0600

    added new page

--- src/components/ProductCategory/Decor/Decor.js
+++ src/components/ProductCategory/Decor/Decor.js
@@ -1,9 +1,19 @@
 import React from 'react';
-import './Decor.css'
+import './Decor.css';
+import img from '../../../assets/Image/category/decore.jpg'
 
 const Decor = () => {
     return (
         <div className='decorContainer'>
+            <a href="product-category/decore/">
+                <div className="decorSub">
+                    <img src={img} alt="" />
+                    <div className="decorInfo">
+                    <p className='title_decor'>decor</p>
+                        <p className='count_decor'>6 products</p>
+                    </div>
+                </div>
+            </a>
             
         </div>
     );
