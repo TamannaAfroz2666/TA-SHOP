2669d23decdaf968ec9bb159b7125f36080058f0
TamannaAfroz2666 <afroztamanna66@gmail.com>
2023-11-09 15:26:21 +0600

    base create

---
+++ src/components/SpringCollection/SpringCollection.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './SpringCollection.css';
+
+const SpringCollection = () => {
+    return (
+        <div className='springCollection'>
+            <div className="springCollSub">
+                
+            </div>
+            
+        </div>
+    );
+};
+
+export default SpringCollection;
