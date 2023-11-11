16f94e969073b60a1846ad906f4f38953155a174
TamannaAfroz2666 <afroztamanna66@gmail.com>
2023-11-12 01:52:43 +0600

    working on cart section

---
+++ src/Page/Cart/CartProducts/CartProducts.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './CartProducts.css';
+
+const CartProducts = () => {
+    return (
+        <div className='cartProducts'>
+            <div className="cartProductsHead">
+                
+            </div>
+            
+        </div>
+    );
+};
+
+export default CartProducts;
