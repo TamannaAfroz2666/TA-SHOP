488b7ab762671afca98faededf5676a4da9a9d38
TamannaAfroz2666 <afroztamanna66@gmail.com>
2023-11-12 00:39:50 +0600

    working on cart section

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import './App.css';
 // import Header from './layout/Header/Header';
 import Main from './layout/Main/Main';
 import Footer from './layout/Footer/Footer';
+import CartMain from './Page/Cart/CartMain/CartMain';
 
 function App() {
   return (
     <div className='app'>
       <Routes>
         <Route path='/' element={ <Main /> }/>
+        <Route path='/cart' element={ <CartMain /> } />
 
       </Routes>
      
---
+++ src/Page/Cart/CartMain/CartMain.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './CartMain.css';
+import Header from '../../../layout/Header/Header';
+
+const CartMain = () => {
+    return (
+        <div className='cartMain'>
+            <Header></Header>
+
+          
+        </div>
+    );
+};
+
+export default CartMain;
