ab6c6713214506966715636febb926619f550b9d
TamannaAfroz2666 <afroztamanna66@gmail.com>
2023-11-12 02:19:10 +0600

    product title and line add

--- src/Page/Cart/CartProducts/CartProducts.js
+++ src/Page/Cart/CartProducts/CartProducts.js
@@ -8,14 +8,29 @@ const CartProducts = () => {
                 <div className="cartProductBody">
                     <h1 className='shoppingTitle'>shopping cart </h1>
                     <div className="cardHrMain">
-                    <hr className='cardHr'/>
+                        <hr className='cardHr' />
                     </div>
                     <p className='spendTitle'>Spend Tk 5,732.00 more and get free shipping!</p>
-                  
+                    <div className="productsCartTitles">
+                        <div className="productTitle1">
+                            <h3 className='productCart_Ti'>product</h3>
+                        </div>
+                        <div className="productTitle2">
+                            <h3 className='productCart_quantity'>quantity</h3>
+                        </div>
+                        <div className="productTitle3">
+                            <h3 className='productCart_Total'>Total</h3>
+                        </div>                      
+
+                    </div>
+                    <div className="cartTitleHr1">
+                            <hr className='titleHr1'/>
+                        </div>
+
                 </div>
 
             </div>
-            
+
         </div>
     );
 };
