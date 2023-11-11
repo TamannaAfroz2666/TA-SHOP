2d196bdbdb815320abdc8c5bb269c1441bbb99ce
TamannaAfroz2666 <afroztamanna66@gmail.com>
2023-11-11 22:30:03 +0600

    links based create

--- src/layout/Footer/Footer.js
+++ src/layout/Footer/Footer.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './Footer.css';
+import { Link } from 'react-router-dom';
 
 const Footer = () => {
     return (
@@ -10,13 +11,29 @@ const Footer = () => {
                         <div className="shopContent">
                             <p className='footerName'>TA</p>
                             <p className='footerSort'>shop</p>
-
+                        </div>
+                        <div className="links_contentFooter">
+                            <h3 className='footerLinkTitle'>links</h3>
+                            <ul className='footerUl'>
+                                <li>
+                                    <Link to='ta-shop/story' className='linkFooterTitle'>Story</Link>
+                                </li>
+                                <li>
+                                    <Link to='ta-shop/contact' className='linkFooterTitle'>Contact</Link>
+                                </li>
+                                <li>
+                                    <Link to='ta-shop/my-accounts/order' className='linkFooterTitle'>truck order</Link>
+                                </li>
+                                <li>
+                                    <Link to='ta-shop/help' className='linkFooterTitle'>help</Link>
+                                </li>
+                            </ul>
                         </div>
                     </div>
                 </div>
-                
+
             </div>
-            
+
         </div>
     );
 };
