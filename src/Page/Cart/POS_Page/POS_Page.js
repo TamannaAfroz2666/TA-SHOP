9fb2f27b66ddec120243d16a0f6247dfc7ca60b0
TamannaAfroz2666 <afroztamanna66@gmail.com>
2023-11-12 16:48:54 +0600

    orking on pos page

---
+++ src/Page/Cart/POS_Page/POS_Page.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './POS_Page.css';
+
+const POS_Page = () => {
+    return (
+        <div className='pos_page'>
+            
+        </div>
+    );
+};
+
+export default POS_Page;
