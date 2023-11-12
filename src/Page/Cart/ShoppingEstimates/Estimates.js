dd8b0d9b742a65d4a55ee7169e0e16859ab122b2
TamannaAfroz2666 <afroztamanna66@gmail.com>
2023-11-12 23:01:13 +0600

    working on new page

---
+++ src/Page/Cart/ShoppingEstimates/Estimates.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Estimates.css';
+
+
+const Estimates = () => {
+    return (
+        <div className='estimates'>
+            
+        </div>
+    );
+};
+
+export default Estimates;
