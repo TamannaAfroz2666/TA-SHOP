89e05cbae8a0c76189d2f87a8a78161f7348d1e9
TamannaAfroz2666 <afroztamanna66@gmail.com>
2023-11-13 11:54:39 +0600

    base create

---
+++ src/components/CheckOut/CheckOut.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './CheckOut.css';
+import { IconName } from "react-icons/ai";
+
+const CheckOut = () => {
+    return (
+        <div className='checkout'>
+            <div className="checkoutSection">
+                <div className="checkoutHead">
+                    <div className="checkTitleSec">
+                    <h3 className='titleCheck'>TA SHOP products</h3>
+                    </div>
+                    <div className="iconCheckSec">
+
+                    </div>
+                  
+
+                </div>
+
+            </div>
+            
+        </div>
+    );
+};
+
+export default CheckOut;
