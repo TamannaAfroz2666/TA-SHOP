8f7a8a70a875f5ce14485061d44f03a2eb97fcba
TamannaAfroz2666 <afroztamanna66@gmail.com>
2023-11-13 10:33:43 +0600

    country name add

--- src/Page/Cart/ShoppingEstimates/Estimates.js
+++ src/Page/Cart/ShoppingEstimates/Estimates.js
@@ -1,8 +1,36 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './Estimates.css';
+// import Select from "react-select";
 
 
 const Estimates = () => {
+    // step 2 start here
+    const [countries, setCountries] = useState([]);
+
+
+    async function fetchCountryData() {
+        try {
+            const response = await fetch('https://restcountries.com/v3.1/all');
+            if (!response.ok) {
+                throw new Error('Network response was not ok.');
+            }
+            const data = await response.json();
+            return data;
+        } catch (error) {
+            // Handle error
+            console.error('Error fetching country data: ', error);
+            return [];
+        }
+    }
+
+    useEffect(() => {
+        async function fetchCountries() {
+            const data = await fetchCountryData();
+            setCountries(data);
+        }
+        fetchCountries();
+    }, []);
+
     return (
         <div className='estimates'>
             <div className="estimatesBody">
@@ -10,7 +38,15 @@ const Estimates = () => {
                     <h3 className='titleEstimate'>Get shipping estimates</h3>
                     <div className="estimatesContents">
                         <div className="country">
-                            <input type="text" />
+                            <select className='countryName'>
+                                {/* <option value="">Select a country</option> */}
+                                {countries.map(country => (
+                                    <option className='optionsEle' key={country.name.common} value={country.name.common}>
+                                        {country.name.common}
+                                    </option>
+                                ))}
+                            </select>
+                           
 
                         </div>
                         <div className="stateContent">
