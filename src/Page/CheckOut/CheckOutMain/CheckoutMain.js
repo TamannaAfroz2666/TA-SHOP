783f4cf942928d5a1dee528f2613426db4c4f36a
TamannaAfroz2666 <afroztamanna66@gmail.com>
2023-11-13 11:41:13 +0600

    working new page

---
+++ src/Page/CheckOut/CheckOutMain/CheckoutMain.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './CheckoutMain.css'
+
+const CheckoutMain = () => {
+    return (
+        <div className='checkoutMain'>
+            
+        </div>
+    );
+};
+
+export default CheckoutMain;
