8a3f340f0c741aa44fc8ac8feb1ced875d71d57a
TamannaAfroz2666 <afroztamanna66@gmail.com>
2023-11-13 13:49:52 +0600

    title designed

--- src/components/CheckOut/CheckOut.js
+++ src/components/CheckOut/CheckOut.js
@@ -13,13 +13,30 @@ const CheckOut = () => {
                         <h3 className='titleCheck'>TA SHOP products</h3>
                     </div>
                     <div className="iconCheckSec">
-                        <Link to= '/cart'>
-                        <BsBag color='tomato' size={25} className='bsBagIcon' />
+                        <Link to='/cart'>
+                            <BsBag color='tomato' size={25} className='bsBagIcon' />
                         </Link>
-                       
                     </div>
+                </div>
+                <hr />
+                <div className="checkOutContent">
+                    <div className="infoCheckOut">
+                        <div className="titleInfoSec">
+                            <div className="conSec1">
+                                <h3 className='conTitle'>Contact</h3>
+                            </div>
+                            <div className="subLog">
+                                <p className='accountTitle'>Have an account?
+                                    <span className='spLog'><a href="/login-page">Log in</a></span>
+                                </p>
+                            </div>
 
+                        </div>
 
+                    </div>
+                    <div className="productCheckOut">
+
+                    </div>
                 </div>
 
             </div>
