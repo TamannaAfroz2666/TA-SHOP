e075ec47ee4db910607b80e879ae6248a0b0844f
TamannaAfroz2666 <afroztamanna66@gmail.com>
2023-11-14 14:13:35 +0600

    working on new page

---
+++ src/Page/ShowAllProducts/MainShowAll/MainShowAll.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './MainShowAll.css'
+
+const MainShowAll = () => {
+    return (
+        <div className='showAllContainer'>
+            
+        </div>
+    );
+};
+
+export default MainShowAll;
