f1e534a4bd6e98ca49260ea1c425e531b1103b5a
TamannaAfroz2666 <afroztamanna66@gmail.com>
2023-11-14 17:03:16 +0600

    base create

--- src/Page/ShowAllProducts/MainShowAll/MainShowAll.js
+++ src/Page/ShowAllProducts/MainShowAll/MainShowAll.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import './MainShowAll.css'
-import Header from '../../../layout/Header/Header.js'
+import Header from '../../../layout/Header/Header.js';
+import { Link } from 'react-router-dom';
+import { IoIosArrowForward } from "react-icons/io";
+
 
 
 const MainShowAll = () => {
@@ -12,6 +15,19 @@ const MainShowAll = () => {
             <div className="bodyShowAll">
                 <div className="titleContentShow">
                     <h1 className='productsTi'>Products</h1>
+                    <div className="linkUl">
+                        <ul className='ulContent'>
+                            <li>
+                                <Link to ='/' className='LinkHome'>Home</Link>
+                            </li>
+                            <li>
+                            <IoIosArrowForward />
+                            </li>
+                            <li>
+                                <p className='title_p'>  Products</p>
+                            </li>
+                        </ul>
+                    </div>
                 </div>
 
             </div>
