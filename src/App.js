f9fee7bfed82c0ddd13a435ce2d134085686da6c
TamannaAfroz2666 <afroztamanna66@gmail.com>
2023-11-14 16:16:31 +0600

    title ad

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Main from './layout/Main/Main';
 import Footer from './layout/Footer/Footer';
 import CartMain from './Page/Cart/CartMain/CartMain';
 import CheckoutMain from './Page/CheckOut/CheckOutMain/CheckoutMain';
+import MainShowAll from './Page/ShowAllProducts/MainShowAll/MainShowAll';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
         <Route path='/' element={ <Main /> }/>
         <Route path='/cart' element={ <CartMain /> } />
         <Route path='/cartCheckOut' element={ <CheckoutMain />} />
+        <Route path='/show-all-product' element={ <MainShowAll/> }/>
 
       </Routes>
      
--- src/Page/ShowAllProducts/MainShowAll/MainShowAll.js
+++ src/Page/ShowAllProducts/MainShowAll/MainShowAll.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import './MainShowAll.css'
+import Header from '../../../layout/Header/Header.js'
+
 
 const MainShowAll = () => {
     return (
         <div className='showAllContainer'>
-            
+            <div className="headerSec">
+                <Header></Header>
+            </div>
+            <div className="bodyShowAll">
+                <div className="titleContentShow">
+                    <h1 className='productsTi'>Products</h1>
+                </div>
+
+            </div>
+
+
         </div>
     );
 };
