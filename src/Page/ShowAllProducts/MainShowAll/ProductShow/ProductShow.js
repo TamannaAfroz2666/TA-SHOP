0f94dcf930b85d8e80ea8f93be3f6d4878fcdffb
TamannaAfroz2666 <afroztamanna66@gmail.com>
2023-11-15 12:41:41 +0600

    working on new page

---
+++ src/Page/ShowAllProducts/MainShowAll/ProductShow/ProductShow.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './ProductShow.css';
+
+const ProductShow = () => {
+    return (
+        <div className='productShowAll'> 
+            
+        </div>
+    );
+};
+
+export default ProductShow;
