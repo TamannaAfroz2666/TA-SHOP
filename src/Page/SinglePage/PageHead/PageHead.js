165611d389924e9365ee1d6757b3f6e7bf69848a
TamannaAfroz2666 <afroztamanna66@gmail.com>
2023-11-15 17:52:43 +0600

    new page working

---
+++ src/Page/SinglePage/PageHead/PageHead.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './PageHead.css'
+
+const PageHead = () => {
+    return (
+        <div className='pageHead'>
+            
+        </div>
+    );
+};
+
+export default PageHead;
---
+++ src/Page/SinglePage/SinglePage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './SinglePage.css';
+
+const SinglePage = () => {
+    return (
+        <div className='singlePage'>
+            <div className="singlePageH">
+                
+            </div>
+            
+        </div>
+    );
+};
+
+export default SinglePage;
