fc5f1d5b2edb68bbee3d3104917b3811e6463955
TamannaAfroz2666 <afroztamanna66@gmail.com>
2023-11-16 20:19:58 +0600

    compare page add

--- src/Page/SinglePage/SubPage/Compare/Compare.js
+++ src/Page/SinglePage/SubPage/Compare/Compare.js
@@ -1,9 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Compare.css';
+import Modal from '@mui/material/Modal';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import { RiArrowUpDownLine } from "react-icons/ri";
 
 const Compare = () => {
+     // compare modal activity 
+     const [openCompare, setOpenCompare] = useState(false);
+     const handleCloseCompare = () => setOpenCompare(false);
+     const compareShow = () => {
+         console.log('cart  going to modal');
+         setOpenCompare(true)
+     }
+
+
     return (
         <div>
+            <div className="size-sec">
+                            <button className='btnSizeInfo' onClick={compareShow}>
+                                <RiArrowUpDownLine size={22} className='iconMeas' />Compare
+                                <hr className='btnHrProInfo' />
+                            </button>
+                            <Modal
+                                className='modalArea1'
+                                open={openCompare}
+                                onClose={handleCloseCompare}
+                                aria-labelledby="modal-modal-title"
+                                aria-describedby="modal-modal-description"
+                            >
+                                <Box className='styleBox'>
+
+                                    <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+                                        <div className="quickViewContent">
+                                            <div className="imageView">
+                                                {/* <img src={img1} alt="" /> */}
+                                                <h1>hhh</h1>
+                                            </div>
+                                            <div className="infoView">
+                                            </div>
+                                        </div>
+                                    </Typography>
+                                </Box>
+                            </Modal>
+
+                        </div>
             
         </div>
     );
--- src/components/SinglePage/SinglePeoduct/SingleProductInfo/SingleProductInfo.js
+++ src/components/SinglePage/SinglePeoduct/SingleProductInfo/SingleProductInfo.js
@@ -12,6 +12,7 @@ import Modal from '@mui/material/Modal';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import { Link } from 'react-router-dom';
+import Compare from '../../../../Page/SinglePage/SubPage/Compare/Compare';
 
 
 const SingleProductInfo = () => {
@@ -221,35 +222,7 @@ const SingleProductInfo = () => {
                 {/* /* basic question list start(delivery,share) */}
                 <div className="basicListPro">
                     <div className="compare-sec">
-                        <div className="size-sec">
-                            <button className='btnSizeInfo' onClick={compareShow}>
-                                <RiArrowUpDownLine size={22} className='iconMeas' />Compare
-                                <hr className='btnHrProInfo' />
-                            </button>
-                            <Modal
-                                className='modalArea1'
-                                open={openCompare}
-                                onClose={handleCloseCompare}
-                                aria-labelledby="modal-modal-title"
-                                aria-describedby="modal-modal-description"
-                            >
-                                <Box className='styleBox'>
-
-                                    <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-                                        <div className="quickViewContent">
-                                            <div className="imageView">
-                                                {/* <img src={img1} alt="" /> */}
-                                                <h1>hhh</h1>
-                                            </div>
-                                            <div className="infoView">
-                                            </div>
-                                        </div>
-                                    </Typography>
-                                </Box>
-                            </Modal>
-
-                        </div>
-
+                        <Compare></Compare>
                     </div>
                     <div className="ask-qu-sec">
                         <div className="size-sec">
