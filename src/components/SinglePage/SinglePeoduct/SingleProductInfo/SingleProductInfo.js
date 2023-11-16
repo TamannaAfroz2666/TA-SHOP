d8a5235877505fe1ef33891d1e4c2f890897c835
TamannaAfroz2666 <afroztamanna66@gmail.com>
2023-11-16 12:32:32 +0600

    basic ttitle add

--- src/components/SinglePage/SinglePeoduct/SingleProductInfo/SingleProductInfo.js
+++ src/components/SinglePage/SinglePeoduct/SingleProductInfo/SingleProductInfo.js
@@ -2,6 +2,9 @@ import React from 'react';
 import './SingleProductInfo.css';
 import { AiFillStar } from "react-icons/ai";
 import { FaRegStarHalfStroke } from "react-icons/fa6";
+import { CiCircleCheck } from "react-icons/ci";
+import { FaFireAlt } from "react-icons/fa";
+import { LuEye } from "react-icons/lu";
 
 const SingleProductInfo = () => {
     return (
@@ -19,7 +22,7 @@ const SingleProductInfo = () => {
                     <p className='oneItem'>(2)</p>
                 </div>
             </div>
-             {/* icon section end */}
+            {/* icon section end */}
             <h3 className='pro-info-title'>Art print T-shirt</h3>
             {/* price section  */}
             <div className="priceSec-pro-info">
@@ -28,8 +31,30 @@ const SingleProductInfo = () => {
                     <p className='amount-sin-pro'>Tk 5,000 </p>
                 </div>
             </div>
-              {/* price section end */}
+            {/* price section end */}
             <span className='byCls'>By <a href="/collection/akajara-store">Akaza Store </a></span>
+            <hr className='hr-sin-pro-in' />
+            <div className="basicList">
+                <div className="stock-info">
+                    <CiCircleCheck color='green' size={16} />
+                    <p className='inStock'>In stock</p>
+                </div>
+                <div className="soldInfo">
+                    <div className="soldPro">
+                        <FaFireAlt color='red' size={18} />
+                        <p className='inStock'><strong>36</strong> sold in last <strong>13</strong> hours</p>
+
+                    </div>
+                    
+
+                </div>
+                <div className="visit">
+                    <LuEye color='black' size={18} />
+                        <p className='inStock'><strong>30</strong>  people are viewing this right now</p>
+
+                    </div>
+
+            </div>
 
 
 
