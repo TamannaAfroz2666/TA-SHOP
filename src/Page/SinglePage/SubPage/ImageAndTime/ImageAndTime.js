239730cf40e1dcbffa35bb8ca445e8d23b90ca21
TamannaAfroz2666 <afroztamanna66@gmail.com>
2023-11-16 22:46:48 +0600

    single product info page done

--- src/Page/SinglePage/SubPage/ImageAndTime/ImageAndTime.js
+++ src/Page/SinglePage/SubPage/ImageAndTime/ImageAndTime.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './ImageAndTime.css';
 import { CiDeliveryTruck } from "react-icons/ci";
+import img1 from '../../../../assets/Image/Card/visaCard.webp'
 const ImageAndTime = () => {
     return (
         <div className='imageAndTime'>
             <hr className='hr-sin-pro-in' />
+            {/* delivery date located */}
             <div className="pro-hand-over">
                 <div className="car-sec">
                     <CiDeliveryTruck size={30} className='carIcon' />
@@ -14,6 +16,13 @@ const ImageAndTime = () => {
                 </div>
             </div>
             <hr className='hr-sin-pro-in' />
+            <div className="card-company">
+                <h5 className='title'>GUARANTEE SAFE & SECURE CHECKOUT</h5>
+                <div className="image-card">
+                    <img src={img1} alt="" />
+
+                </div>
+            </div>
 
         </div>
     );
