03be98cdd3255b2d9ca8dc9211c3b1e6ab546a61
TamannaAfroz2666 <afroztamanna66@gmail.com>
2023-11-16 22:51:27 +0600

    working new page of description section

---
+++ src/components/SinglePage/ProductDesceiption/ProductDescription.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './ProductDescription.css';
+
+const ProductDescription = () => {
+    return (
+        <div className='pro-des-main'>
+            <div className="des-sub">
+                
+            </div>
+            
+        </div>
+    );
+};
+
+export default ProductDescription;
