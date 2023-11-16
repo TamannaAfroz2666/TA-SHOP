593c96336bef53c28565970b4ee1cacc2b03c37a
TamannaAfroz2666 <afroztamanna66@gmail.com>
2023-11-16 22:02:26 +0600

    working on new page

---
+++ src/Page/SinglePage/SubPage/ImageAndTime/ImageAndTime.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './ImageAndTime.css'
+
+const ImageAndTime = () => {
+    return (
+        <div className='imageAndTime'>
+            
+        </div>
+    );
+};
+
+export default ImageAndTime;
