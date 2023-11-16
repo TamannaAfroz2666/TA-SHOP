348ddd3e87f4c84947dde4688466ea77fd061712
TamannaAfroz2666 <afroztamanna66@gmail.com>
2023-11-16 22:30:57 +0600

    delivery time date cleared

--- src/Page/SinglePage/SubPage/ImageAndTime/ImageAndTime.js
+++ src/Page/SinglePage/SubPage/ImageAndTime/ImageAndTime.js
@@ -4,14 +4,17 @@ import { CiDeliveryTruck } from "react-icons/ci";
 const ImageAndTime = () => {
     return (
         <div className='imageAndTime'>
-             <hr className='hr-sin-pro-in' />
-             <div className="pro-hand-over">
+            <hr className='hr-sin-pro-in' />
+            <div className="pro-hand-over">
                 <div className="car-sec">
-                <CiDeliveryTruck size={30} className='carIcon'/>
-
+                    <CiDeliveryTruck size={30} className='carIcon' />
                 </div>
-             </div>
-            
+                <div className="deliveryTime">
+                    <p className='orderInCls'>Order in the next 12 hours 46 minutes to get it between <span className='dateDelStart'> Tuesday, Nov 21 </span> and <span className='dateDelStart'>Saturday, Nov 25</span></p>
+                </div>
+            </div>
+            <hr className='hr-sin-pro-in' />
+
         </div>
     );
 };
