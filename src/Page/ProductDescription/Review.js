0cff6fd86f352215b31d1f891533e61caab5b25b
TamannaAfroz2666 <afroztamanna66@gmail.com>
2023-11-18 11:55:29 +0600

    review page done

--- src/Page/ProductDescription/Review.js
+++ src/Page/ProductDescription/Review.js
@@ -2,6 +2,8 @@ import React from 'react';
 import './Review.css';
 import { AiFillStar } from "react-icons/ai";
 import { FaRegStar } from "react-icons/fa";
+import { Link } from 'react-router-dom';
+
 
 
 const Review = () => {
@@ -11,30 +13,56 @@ const Review = () => {
                 <h3 className='cus-review'>Customer Reviews</h3>
                 <div className="review-part">
                     <div className="icon-review">
-                        <div className="icons">
-                            {/* icon section start  */}
-                            <div className="iconsField1">
-                                <div className="iconsRating">
-                                    <span className='hhh'><AiFillStar size={18} color='orange' /></span>
-                                    <span><AiFillStar size={18} color='orange' /></span>
-                                    <span><AiFillStar size={18} color='orange' /></span>
-                                    <span><FaRegStar size={17} className='spIcon' /></span>
-                                    <span><FaRegStar size={17} className='spIcon' /></span>
-                                </div>
-                                <div className="numProduct">
-                                    <p className='base-on'>Based on 1 review
-                                    </p>
-                                </div>
+                        {/* <div className="icons"> */}
+                        {/* icon section start  */}
+                        <div className="iconsField1">
+                            <div className="iconsRating">
+                                <span className='hhh'><AiFillStar size={18} color='orange' /></span>
+                                <span><AiFillStar size={18} color='orange' /></span>
+                                <span><AiFillStar size={18} color='orange' /></span>
+                                <span><FaRegStar size={17} className='spIcon' /></span>
+                                <span><FaRegStar size={17} className='spIcon' /></span>
                             </div>
-                            {/* icon section end */}
-
+                            <div className="numProduct">
+                                <p className='base-on'>Based on 1 review
+                                </p>
+                            </div>
                         </div>
-                        
+                        {/* icon section end */}
 
+                        {/* </div> */}
                     </div>
                     <div className="btn-review">
                         <button className='reviewBtnCls'>Write a review</button>
                     </div>
+                </div>
+                <div className="review-des">
+                    <div className="icons">
+                        {/* icon section start  */}
+                        <div className="iconsField1">
+                            <div className="iconsRating">
+                                <span className='hhh'><AiFillStar size={18} color='orange' /></span>
+                                <span><AiFillStar size={18} color='orange' /></span>
+                                <span><AiFillStar size={18} color='orange' /></span>
+                                <span><FaRegStar size={17} className='spIcon' /></span>
+                                <span><FaRegStar size={17} className='spIcon' /></span>
+                            </div>
+                            <div className="numProduct">
+                                <p className='base-on'>
+                                    1 on Sep 29, 2023
+                                </p>
+                            </div>
+                        </div>
+                        {/* icon section end */}
+                        <h5 className='text-review'>Text</h5>
+                        <span className='text-sub'>Text</span>
+                    </div>
+                    <div className="reviews-link">
+                        <Link to='reviews/report-process'>
+                            Report as Inappropriate</Link>
+                    </div>
+
+
                 </div>
 
 
