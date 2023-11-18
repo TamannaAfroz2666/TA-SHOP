c0e21d8b78ee9d6affa2dc72d8b2417b7674f316
TamannaAfroz2666 <afroztamanna66@gmail.com>
2023-11-18 13:08:19 +0600

    title add

--- src/Page/SinglePage/SinglePage.js
+++ src/Page/SinglePage/SinglePage.js
@@ -4,24 +4,26 @@ import Header from '../../layout/Header/Header';
 import PageHead from './PageHead/PageHead';
 import SingleProduct from '../../components/SinglePage/SinglePeoduct/SingleProduct';
 import ProductDescription from '../../components/SinglePage/ProductDesceiption/ProductDescription';
+import AdditionalProduct from '../../components/AdditionalProduct/AdditionalProduct';
 
 const SinglePage = () => {
     return (
         <div className='singlePage'>
             <div className="singlePageHead">
-                <Header></Header>
+                <Header/>
             </div>
             <div className="single-pageContent">
                 <div className="subPageHead">
-                    <PageHead></PageHead>
+                    <PageHead/>
                 </div>
                 <div className="singleProdCom">
-                    <SingleProduct></SingleProduct>
+                    <SingleProduct/>
                 </div>
                 <div className="productDescription">
-                    <ProductDescription></ProductDescription>
+                    <ProductDescription/>
                 </div>
                 <div className="additionalProduct">
+                    <AdditionalProduct/>
 
                 </div>
 
--- src/components/AdditionalProduct/AdditionalProduct.js
+++ src/components/AdditionalProduct/AdditionalProduct.js
@@ -4,6 +4,12 @@ import './AdditionalProduct.css';
 const AdditionalProduct = () => {
     return (
         <div className='additional'>
+            <div className="additionalSub">
+                <h3 className='additionalTitle'>You may also like</h3>
+                <div className="pageCarrosel">
+                    
+                </div>
+            </div>
             
         </div>
     );
