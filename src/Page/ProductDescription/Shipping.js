bad51c2e6ec958e75859ea54d9a60b3d761ffc9f
TamannaAfroz2666 <afroztamanna66@gmail.com>
2023-11-18 12:28:12 +0600

    shipping page working

---
+++ src/Page/ProductDescription/Shipping.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Shipping.css'
+
+const Shipping = () => {
+    return (
+        <div className='shipping-main'>
+            
+        </div>
+    );
+};
+
+export default Shipping;
