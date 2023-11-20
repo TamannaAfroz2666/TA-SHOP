1fb95917a7d65a73060971759ec63a1f3442ca46
TamannaAfroz2666 <afroztamanna66@gmail.com>
2023-11-20 11:01:35 +0600

    add to cart button decleared

--- src/components/Treanding/TrendingProduct/TrendingProduct.js
+++ src/components/Treanding/TrendingProduct/TrendingProduct.js
@@ -12,6 +12,7 @@ import Typography from '@mui/material/Typography';
 import Modal from '@mui/material/Modal';
 import { FiMinus } from "react-icons/fi";
 import { GoPlus } from "react-icons/go";
+import { IoBagHandleSharp } from "react-icons/io5";
 
 
 
@@ -183,15 +184,18 @@ const TrendingProduct = () => {
                                                     <button className='minus-btn'>
                                                     <FiMinus className='minus-trending'/>
                                                     </button>
-                                                    <span>1</span>
+                                                    <span className='one-trending'
+                                                    >1</span>
                                                     <button className='plus-btn'>
                                                     <GoPlus className='minus-trending'/>
                                                     </button>
 
-
-
                                                 </div>
                                                 <div className="cartAdd">
+                                                    <button className='card-btn-cls'>
+                                                    <IoBagHandleSharp size={18}/>
+                                                       <span className='cart-icon'>add to cart</span> 
+                                                    </button>
 
                                                 </div>
                                                 {/* <p className='selectOptionTitle'>
