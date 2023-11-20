8222f947da2fb328fb018509f9b61d740a3abb10
TamannaAfroz2666 <afroztamanna66@gmail.com>
2023-11-20 17:25:00 +0600

    add base on gift warp page

---
+++ src/Page/TrendingProduct/CartGiftWarp/CartGiftWarp.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import './CartGiftWarp.css';
+
+const CartGiftWarp = () => {
+    return (
+        <div className='CartGiftWarp'>
+            <div className="card-gifts-content">
+                <div className="add-gift">
+
+                </div>
+                <div className="add-note">
+
+                </div>
+                <div className="add-coupon">
+
+                </div>
+                <div className="add-shipping">
+                    
+                </div>
+
+            </div>
+            
+        </div>
+    );
+};
+
+export default CartGiftWarp;
