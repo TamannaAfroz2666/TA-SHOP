fce84626a58ed3db2218dfe1635ff76bc9311d46
TamannaAfroz2666 <afroztamanna66@gmail.com>
2023-11-20 14:10:44 +0600

    add hr line

--- src/components/Treanding/TrendingProduct/AddToCard/AddToCard.js
+++ src/components/Treanding/TrendingProduct/AddToCard/AddToCard.js
@@ -30,15 +30,15 @@ const AddToCard = () => {
                     <span className='cart-icon'>add to cart</span>
                 </button>
 
-                <div className="offcanvas offcanvas-end" tabindex="-1" id="offcanvasRight" aria-labelledby="offcanvasRightLabel">
+                {/* <div className="offcanvas offcanvas-end" tabindex="-1" id="offcanvasRight" aria-labelledby="offcanvasRightLabel">
                     <div className="offcanvas-header">
-                        {/* <h5 id="offcanvasRightLabel">Shopping Cart (3)</h5> */}
+                        <h5 id="offcanvasRightLabel">Shopping Cart (3)</h5>
                         <button type="button" className="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
                     </div>
                     <div className="offcanvas-body">
                        
                     </div>
-                </div>
+                </div> */}
             </div>
         </div>
 
--- src/layout/Header/SubHeader/SubHeader.js
+++ src/layout/Header/SubHeader/SubHeader.js
@@ -5,7 +5,7 @@ import { BsBasket } from "react-icons/bs";
 const SubHeader = () => {
     return (
         <div>
-              <button type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasRight" aria-controls="offcanvasRight">
+            <button type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasRight" aria-controls="offcanvasRight">
                 <BsBasket color='tomato' size={20} />
             </button>
 
@@ -13,14 +13,21 @@ const SubHeader = () => {
                 <div className="offcanvas-header">
                     <h5 id="offcanvasRightLabel" className='cart-title'>shopping cart (3)</h5>
                     <button type="button" className="btn-close text-reset  " data-bs-dismiss="offcanvas" aria-label="Close" id='btnClose'>
-                       
+
                     </button>
                 </div>
                 <div className="offcanvas-body">
-                    ...
+                    <div className="body-cart">
+                        <p className='spend-cart'>Spend <span className='spend-middle'>Tk 3,317.00 </span>more and get <span className='spend-middle'>free shipping!</span></p>
+                        <hr className='lineHr2' />
+
+                    </div>
+
+
+
                 </div>
             </div>
-            
+
         </div>
     );
 };
