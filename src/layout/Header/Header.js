8714438d016e51736bcd2f4ff3b49e4cfebdb9da
TamannaAfroz2666 <afroztamanna66@gmail.com>
2023-11-20 11:29:10 +0600

    update the header section

--- src/layout/Header/Header.js
+++ src/layout/Header/Header.js
@@ -12,6 +12,8 @@ import 'bootstrap/dist/css/bootstrap.css';
 // // import List from '@mui/material/List';
 // import Divider from '@mui/material/Divider';
 import { motion } from 'framer-motion';
+// import Sub from './sub/sub';
+import SubHeader from './SubHeader/SubHeader';
 
 const Header = () => {
 
@@ -110,43 +112,13 @@ const Header = () => {
                                     <Link className='loginHeadTitle'>Log In</Link>
                                 </div>
                                 <div className="cartSubLink">
-                                    <button type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasRight" aria-controls="offcanvasRight">
-                                    <BsBasket color='tomato' size={20} />
-                                    </button>
-
-                                    <div className="offcanvas offcanvas-end" tabindex="-1" id="offcanvasRight" aria-labelledby="offcanvasRightLabel">
-                                        <div className="offcanvas-header">
-                                            <h5 id="offcanvasRightLabel">Offcanvas right</h5>
-                                            <button type="button" className="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-                                        </div>
-                                        <div className="offcanvas-body">
-                                            ...
-                                        </div>
-                                    </div>
-                                    
-
+                                    <SubHeader />
                                 </div>
-
-
                             </div>
                         </div>
 
                     </div>
-                    {/* <div>
-                        {['top'].map((anchor) => (
-                            <React.Fragment key={anchor}>
-                                <Button onClick={toggleDrawer(anchor, true)}>{anchor}</Button>
-                                <Drawer
-                                    anchor={anchor}
-                                    open={state[anchor]}
-                                    onClose={toggleDrawer(anchor, false)}
-                                >
-                                    {list(anchor)}
-                                </Drawer>
-                            </React.Fragment>
-                        ))}
-                    </div> */}
-
+                
                 </div>
 
             </div>
---
+++ src/layout/Header/SubHeader/SubHeader.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { BsBasket } from "react-icons/bs";
+
+const SubHeader = () => {
+    return (
+        <div>
+              <button type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasRight" aria-controls="offcanvasRight">
+                <BsBasket color='tomato' size={20} />
+            </button>
+
+            <div className="offcanvas offcanvas-end" tabindex="-1" id="offcanvasRight" aria-labelledby="offcanvasRightLabel">
+                <div className="offcanvas-header">
+                    <h5 id="offcanvasRightLabel">Offcanvas right</h5>
+                    <button type="button" className="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                </div>
+                <div className="offcanvas-body">
+                    ...
+                </div>
+            </div>
+            
+        </div>
+    );
+};
+
+export default SubHeader;
