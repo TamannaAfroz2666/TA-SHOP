2bee8b05b21835c8b92cee9efdadbc1b53c35aec
TamannaAfroz2666 <afroztamanna66@gmail.com>
2023-11-23 23:04:27 +0600

    update the page

--- src/Page/WishListHeader/WishListHeader.js
+++ src/Page/WishListHeader/WishListHeader.js
@@ -3,7 +3,6 @@ import './WishListHeader.css';
 import { Link } from 'react-router-dom';
 import { AiOutlineRight } from "react-icons/ai";
 
-
 const WishListHeader = () => {
     return (
         <div className='wishListHeader'>
@@ -11,17 +10,24 @@ const WishListHeader = () => {
                 <h1 className='title'>Wishlist</h1>
                 <div className="ul-ws">
                     <ul className='wishlist-ul'>
-                        <li>
-                            <Link className='ws-li' to='/'>Home</Link>
-                        </li>
-                        <li><AiOutlineRight className='greater-ws' size={13}/></li>
-                        <li className='wl'>Wishlist</li>
+                        <div className="jj">
+                            <li>
+                                <Link className='ws-li' to='/'>Home
+                                    <span className='hr-home'>Home</span>
+                                </Link>
+                            </li>
+                        </div>
+                        <div className="main-sub">
+                            <li><AiOutlineRight className='greater-ws' size={13} /></li>
+                            <li className='wl'>Wishlist</li>
+                        </div>
                     </ul>
-
                 </div>
-
+                <div className="product-show">
+                    <h1 className='title-ws-err'>There are no products in wishlist</h1>
+                </div>
             </div>
-            
+
         </div>
     );
 };
