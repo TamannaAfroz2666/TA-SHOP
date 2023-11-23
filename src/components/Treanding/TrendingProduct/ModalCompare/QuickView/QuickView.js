1f38114935d5e96e15c67d6754798d1025a9aef7
TamannaAfroz2666 <afroztamanna66@gmail.com>
2023-11-23 11:15:58 +0600

    base create

--- src/components/Treanding/TrendingProduct/ModalCompare/QuickView/QuickView.js
+++ src/components/Treanding/TrendingProduct/ModalCompare/QuickView/QuickView.js
@@ -1,14 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './QuickView.css';
 // import img1 from '../../../assets/Image/NewArrivals/pic20.jpg';
 import img1 from '../../../../../assets/Image/NewArrivals/pic20.jpg'
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import Modal from '@mui/material/Modal';
-import {  AiOutlineEye } from "react-icons/ai";
+import { AiOutlineEye, AiFillStar } from "react-icons/ai";
 
 const QuickView = () => {
-    const [open, setOpen] = React.useState(false);
+    const [open, setOpen] = useState(false);
     const handleOpen = () => setOpen(true);
     const handleClose = () => setOpen(false);
     return (
@@ -25,15 +25,27 @@ const QuickView = () => {
                 aria-labelledby="modal-modal-title"
                 aria-describedby="modal-modal-description"
             >
-                <Box className='styleBox'>
-
+                <Box className='styleBoxQuick'>
                     <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-                        <div className="quickViewContent">
-                            <div className="imageView">
+                        <div className="quickViewContent1">
+                            <div className="imageView1">
                                 <img src={img1} alt="" />
-
                             </div>
                             <div className="infoView">
+                                <div className="star-container">
+                                    <div className="iconsField1">
+                                        <div className="iconsRating1">
+                                            <span className='hhh'><AiFillStar size={15} color='orange' /></span>
+                                            <span><AiFillStar size={15} color='orange' /></span>
+                                            <span><AiFillStar size={15} color='orange' /></span>
+                                            <span><AiFillStar size={15} color='orange' /></span>
+                                            <span><AiFillStar size={15} color='orange' /></span>
+                                        </div>
+                                        <div className="numProduct">
+                                            <p className='oneItem'>(1)</p>
+                                        </div>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </Typography>
