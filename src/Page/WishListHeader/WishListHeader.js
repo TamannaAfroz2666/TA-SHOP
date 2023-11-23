febd90f8740cd11653798d218b18e5acf8318bdb
TamannaAfroz2666 <afroztamanna66@gmail.com>
2023-11-23 22:42:25 +0600

    sub link add

--- src/Page/WishListHeader/WishListHeader.js
+++ src/Page/WishListHeader/WishListHeader.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import './WishListHeader.css';
+import { Link } from 'react-router-dom';
+import { AiOutlineRight } from "react-icons/ai";
+
 
 const WishListHeader = () => {
     return (
         <div className='wishListHeader'>
             <div className="subContentWi">
                 <h1 className='title'>Wishlist</h1>
+                <div className="ul-ws">
+                    <ul className='wishlist-ul'>
+                        <li>
+                            <Link className='ws-li' to='/'>Home</Link>
+                        </li>
+                        <li><AiOutlineRight className='greater-ws' size={13}/></li>
+                        <li className='wl'>Wishlist</li>
+                    </ul>
+
+                </div>
 
             </div>
             
