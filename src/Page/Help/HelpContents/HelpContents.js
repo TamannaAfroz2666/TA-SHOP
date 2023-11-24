cfeadd56db8f8d2748cf914b318abb71d4ce764c
TamannaAfroz2666 <afroztamanna66@gmail.com>
2023-11-24 09:12:02 +0600

    base create

--- src/Page/Help/HelpContents/HelpContents.js
+++ src/Page/Help/HelpContents/HelpContents.js
@@ -1,11 +1,22 @@
 import React from 'react';
-import './HelpContents.css'
+import './HelpContents.css';
+import { FaUser } from "react-icons/fa";
+import { FaArrowRightLong } from "react-icons/fa6";
+import { Link } from 'react-router-dom';
+
+
 
 const HelpContents = () => {
     return (
         <div className='helpContents'>
             <div className="contents">
                 <div className="acc-pri">
+                    <div className="acc-privacy">
+                        <span className='main-fa'><FaUser className='fa-title'/></span>
+                        <h4 className='acc-title'>Account Privacy</h4>
+                        <p className='acc-des'>Quisque a pharetra quam. Donec et risus sem. Etiam sollicitudin leo eu congue gravida. In semper lectus neque, eu interdum nisl pretium sit amet etiam efficitur.</p>
+                        <Link className='link-acc' to='/more'>Privacy policy <span className='link-icon-mn'><FaArrowRightLong className='link-icon-sb'/></span></Link>
+                    </div>
 
                 </div>
                 <div className="support-sp">
