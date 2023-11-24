2a100834d69577753ab6eefbcb9888398c4bcd7c
TamannaAfroz2666 <afroztamanna66@gmail.com>
2023-11-24 14:55:12 +0600

    fqa added

--- src/Page/Help/HelpContents/FQAHelp/FQAContents/FQAContents.js
+++ src/Page/Help/HelpContents/FQAHelp/FQAContents/FQAContents.js
@@ -9,34 +9,87 @@ const FQAContents = () => {
     return (
         <div className='fqaContents'>
             <div className="fquContent">
+                <div className="content-sec">
                 <Accordion defaultActiveKey="0">
                     <Accordion.Item eventKey="0">
-                        <Accordion.Header>
-                            <h1>How do I know that my products lorem ipsum dolor sit?</h1>
+                        <Accordion.Header className='head-acc'>
+                            <h1 className='head-title'>How do I know that my products lorem ipsum dolor sit?</h1>
                         </Accordion.Header>
                         <Accordion.Body>
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-                            minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-                            aliquip ex ea commodo consequat. Duis aute irure dolor in
-                            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-                            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-                            culpa qui officia deserunt mollit anim id est laborum.
+                            <div className="acc-body">
+                                <p className='acc-des'>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                                    eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+                                    minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                                    aliquip ex ea commodo consequat.
+                                </p>
+                            </div>
                         </Accordion.Body>
                     </Accordion.Item>
                     <Accordion.Item eventKey="1">
-                        <Accordion.Header>Accordion Item #2</Accordion.Header>
+                        <Accordion.Header className='head-acc'>
+                            <h1 className='head-title1'>Where can I see my invoices?</h1>
+                        </Accordion.Header>
                         <Accordion.Body>
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-                            minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-                            aliquip ex ea commodo consequat. Duis aute irure dolor in
-                            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-                            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-                            culpa qui officia deserunt mollit anim id est laborum.
+                            <div className="acc-body">
+                                <p className='acc-des'>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                                    eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+                                    minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                                    aliquip ex ea commodo consequat.
+                                </p>
+                            </div>
+                        </Accordion.Body>
+                    </Accordion.Item>
+                    <Accordion.Item eventKey="2">
+                        <Accordion.Header className='head-acc'>
+                            <h1 className='head-title1'>Can I compare prices between tincidunt dui in congue?</h1>
+                        </Accordion.Header>
+                        <Accordion.Body>
+                            <div className="acc-body">
+                                <p className='acc-des'>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                                    eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+                                    minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                                    aliquip ex ea commodo consequat.
+                                </p>
+                            </div>
+                        </Accordion.Body>
+                    </Accordion.Item>
+                    <Accordion.Item eventKey="3">
+                        <Accordion.Header className='head-acc'>
+                            <h1 className='head-title1'>Are there any surcharges?</h1>
+                        </Accordion.Header>
+                        <Accordion.Body>
+                            <div className="acc-body">
+                                <p className='acc-des'>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                                    eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+                                    minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                                    aliquip ex ea commodo consequat.
+                                </p>
+                            </div>
+                        </Accordion.Body>
+                    </Accordion.Item>
+                    <Accordion.Item eventKey="4">
+                        <Accordion.Header className='head-acc'>
+                            <h1 className='head-title1'>I made an online purchase. Where can I check order status?</h1>
+                        </Accordion.Header>
+                        <Accordion.Body>
+                            <div className="acc-body">
+                                <p className='acc-des'>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                                    eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+                                    minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                                    aliquip ex ea commodo consequat.
+                                </p>
+                            </div>
                         </Accordion.Body>
                     </Accordion.Item>
                 </Accordion>
+
+                </div>
+              
 
 
 
