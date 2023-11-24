9e263e1a117e079300c52e5c33a2feeb00fe7ccf
TamannaAfroz2666 <afroztamanna66@gmail.com>
2023-11-24 09:06:14 +0600

    new page working

---
+++ src/Page/Help/HelpContents/HelpContents.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './HelpContents.css'
+
+const HelpContents = () => {
+    return (
+        <div className='helpContents'>
+            <div className="contents">
+                <div className="acc-pri">
+
+                </div>
+                <div className="support-sp">
+
+                </div>
+                <div className="seller-stand">
+
+                </div>
+            </div>
+            
+        </div>
+    );
+};
+
+export default HelpContents;
