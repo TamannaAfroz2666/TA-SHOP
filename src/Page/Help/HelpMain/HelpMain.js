44a3d772447dad706a612735119b38feb8e3dfd0
TamannaAfroz2666 <afroztamanna66@gmail.com>
2023-11-24 09:40:40 +0600

    base design created

--- src/Page/Help/HelpMain/HelpMain.js
+++ src/Page/Help/HelpMain/HelpMain.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './HelpMain.css';
 import Header from '../../../layout/Header/Header';
 import HelpHeader from '../HelpHeader';
+import HelpContents from '../HelpContents/HelpContents';
 
 const HelpMain = () => {
     return (
@@ -11,6 +12,9 @@ const HelpMain = () => {
             </div>
             <div className="sub-help-header">
                 <HelpHeader/>
+            </div>
+            <div className="content-helps">
+                <HelpContents />
             </div>
             
         </div>
