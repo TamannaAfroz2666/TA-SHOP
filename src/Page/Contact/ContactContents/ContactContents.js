5bb1ecee25b96add72d0c7894146a599d6a8d752
TamannaAfroz2666 <afroztamanna66@gmail.com>
2023-11-25 00:45:38 +0600

    contact items page done

--- src/Page/Contact/ContactContents/ContactContents.js
+++ src/Page/Contact/ContactContents/ContactContents.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import './ContactContents.css';
 import { IoCall } from "react-icons/io5";
-// import { FaArrowRightLong } from "react-icons/fa6";
-import { BiSupport } from "react-icons/bi";
-import { HiStar } from "react-icons/hi2";
-import { Link } from 'react-router-dom';
+import { MdOutlineMail } from "react-icons/md";
+import { FaLocationDot } from "react-icons/fa6";
+
 
 const ContactContents = () => {
     return (
@@ -20,17 +19,17 @@ const ContactContents = () => {
                     </div>
                     <div className="email-acc">
                         <div className="acc-privacy">
-                            <span className='main-fa-contact'><IoCall className='fa-title' size={60} /></span>
-                            <h4 className='acc-title-contact'>Phone Number</h4>
-                            <h3 className='acc-des-contact'>929-242-6868.</h3>
+                            <span className='main-fa-contact'><MdOutlineMail className='fa-title' size={60} /></span>
+                            <h4 className='acc-title-contact'>Email</h4>
+                            <h3 className='acc-des-contact'>contact@info.com</h3>
                         </div>
 
                     </div>
                     <div className="address-acc">
                         <div className="acc-privacy">
-                            <span className='main-fa-contact'><IoCall className='fa-title' size={60} /></span>
-                            <h4 className='acc-title-contact'>Phone Number</h4>
-                            <h3 className='acc-des-contact'>929-242-6868.</h3>
+                            <span className='main-fa-contact'><FaLocationDot className='fa-title' size={60} /></span>
+                            <h4 className='acc-title-contact'>Address</h4>
+                            <h3 className='acc-des-contact'>123 Fifth Avenue, New York, NY 10160</h3>
                         </div>
                     </div>
                 </div>
