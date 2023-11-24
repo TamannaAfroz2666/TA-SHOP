c6be0be262f111098dbbe12a416ec338fbd4100d
TamannaAfroz2666 <afroztamanna66@gmail.com>
2023-11-24 14:11:47 +0600

    add base

--- src/Page/Help/HelpContents/FQAHelp/FQAContents/FQAContents.js
+++ src/Page/Help/HelpContents/FQAHelp/FQAContents/FQAContents.js
@@ -1,18 +1,43 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './FQAContents.css';
-import { Accordion, Icon } from 'semantic-ui-react'
-import { render } from '@testing-library/react';
+import Accordion from 'react-bootstrap/Accordion';
 
 
 
 const FQAContents = () => {
-   
-
 
     return (
         <div className='fqaContents'>
             <div className="fquContent">
-               
+                <Accordion defaultActiveKey="0">
+                    <Accordion.Item eventKey="0">
+                        <Accordion.Header>
+                            <h1>How do I know that my products lorem ipsum dolor sit?</h1>
+                        </Accordion.Header>
+                        <Accordion.Body>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+                            minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                            aliquip ex ea commodo consequat. Duis aute irure dolor in
+                            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+                            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+                            culpa qui officia deserunt mollit anim id est laborum.
+                        </Accordion.Body>
+                    </Accordion.Item>
+                    <Accordion.Item eventKey="1">
+                        <Accordion.Header>Accordion Item #2</Accordion.Header>
+                        <Accordion.Body>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+                            minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                            aliquip ex ea commodo consequat. Duis aute irure dolor in
+                            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+                            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+                            culpa qui officia deserunt mollit anim id est laborum.
+                        </Accordion.Body>
+                    </Accordion.Item>
+                </Accordion>
+
 
 
 
