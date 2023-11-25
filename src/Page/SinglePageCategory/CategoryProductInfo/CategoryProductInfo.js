bb6629a5efc18a47a7ac8336b243db57ed513345
TamannaAfroz2666 <afroztamanna66@gmail.com>
2023-11-26 02:07:13 +0600

    sale section add

--- src/Page/SinglePageCategory/CategoryProductInfo/CategoryProductInfo.js
+++ src/Page/SinglePageCategory/CategoryProductInfo/CategoryProductInfo.js
@@ -1,13 +1,30 @@
 import React from 'react';
 import './CategoryProductInfo.css';
-import im1 from '../../../assets/Image/category/office.jpg'
+import im1 from '../../../assets/Image/category/office.jpg';
+// import { Link } from 'react-router-dom';
+import Button from '@mui/material/Button';
+import { IoSearchSharp } from "react-icons/io5";
 
 const CategoryProductInfo = () => {
     return (
         <div className='CategoryProductInfo'>
             <div className="cateProContents">
                 <div className="imageCateSection">
-                    <img src={im1} alt="" />
+                    <div className="saleSearchSec">
+                        <div className="saleCategorySec">
+                            <button className='saleBtnDe' >sale!</button>
+                        </div>
+                        <div className="eyeContact1">
+                            <Button > <span className='linkViewBtn'><IoSearchSharp className='iconDeEye' size={20} color='black' /> </span>
+                            </Button>
+
+
+                        </div>
+                    </div>
+                    <div className="imgCateSec">
+                        <img src={im1} alt="" />
+                    </div>
+
 
                 </div>
                 <div className="infoCateSection">
@@ -16,7 +33,7 @@ const CategoryProductInfo = () => {
 
             </div>
 
-            
+
         </div>
     );
 };
