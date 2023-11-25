93026607861503c194d304022c9118090555c038
TamannaAfroz2666 <afroztamanna66@gmail.com>
2023-11-25 12:56:56 +0600

    founder info section done

--- src/Page/AboutUs/StoryHeader/StoryContent/FounderInfo/FounderInfo.js
+++ src/Page/AboutUs/StoryHeader/StoryContent/FounderInfo/FounderInfo.js
@@ -16,6 +16,24 @@ const FounderInfo = () => {
                 <div className="founderTitleHead">
                     <FounderTitle/>
                 </div>
+                <div className="founderSection">
+                    <div className="infoSection">
+                        <h3 className='aboutFounder'>About the founder</h3>
+                        <div className="hrFounder">
+                            <hr className='hrFounderTi'/>
+                        </div>
+                        <p className='founderDes'>
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores quaerat, itaque soluta officiis nostrum voluptates cupiditate pariatur! Sit similique, quam veritatis amet totam ullam porro quaerat voluptatum, ducimus quisquam itaque.
+                        </p>
+                        <p className='founderDes'>Nam rutrum mattis velit eget volutpat. Fusce egestas mi urna, id pulvinar ipsum dictum eget.</p>
+
+                    </div>
+                    <div className="imgSection">
+
+                    </div>
+
+
+                </div>
             </div>
 
         </div>
