7d92be065aeb88634b31050e94e358adf962f6b4
TamannaAfroz2666 <afroztamanna66@gmail.com>
2023-11-25 17:28:28 +0600

    page sorting done

--- src/Page/DecorPage/DecorContentMain/DecorContentMain.js
+++ src/Page/DecorPage/DecorContentMain/DecorContentMain.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './DecorContentMain.css'
+import { IoChevronDown } from "react-icons/io5";
 
 const DecorContentMain = () => {
     return (
@@ -9,20 +10,18 @@ const DecorContentMain = () => {
                     <h4 className='showingAll'>Showing all 9 results</h4>
                 </div>
                 <div className="sorting-sec">
-                    <div className="dropFilter">
-                        <select name="language" className='selectDrop' id="language">
-                            <option value="javascript">Best Selling</option>
-                            <option value="python">Featured</option>
-                            <option value="java" selected>Alphabetically, A-Z</option>
-                            <option value="c++">Alphabetically, Z-A</option>
-                            <option value="javascript">Price, Low-High</option>
-                            <option value="python">Price,High-Low</option>
-                            <option value="c++">Date, Old-New</option>
-                            <option value="java">Date, New-Old</option>
+                    <div className="dropFilterDecor">
+                        <select name="language" className='selectDropDecor' id="language">
+                            <option value="default" selected className='optionDecorSorting'>Default Sorting</option>
+                            <option value="popularity" className='optionDecorSorting'>Sort by popularity</option>
+                            <option value="java" className='optionDecorSorting'>Sort by average rating</option>
+                            <option value="c++" className='optionDecorSorting'>Sort by latest</option>
+                            <option value="javascript" className='optionDecorSorting'>Sort by price: low-high</option>
+                            <option value="python" className='optionDecorSorting'>Sort by price: high-low</option>
                         </select>
-                        {/* <label htmlFor="language">
-                        <span className='faSort'><FaSortAmountDownAlt /></span>
-                    </label> */}
+                        <label htmlFor="language">
+                        <span className='faSortDecor'><IoChevronDown /></span>
+                    </label>
 
                     </div>
 
