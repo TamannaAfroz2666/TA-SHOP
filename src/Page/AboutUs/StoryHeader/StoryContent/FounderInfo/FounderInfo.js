ccdbac4a73fa7f0990f88b7349135be33a940632
TamannaAfroz2666 <afroztamanna66@gmail.com>
2023-11-25 12:44:10 +0600

    title done

--- src/Page/AboutUs/StoryHeader/StoryContent/FounderInfo/FounderInfo.js
+++ src/Page/AboutUs/StoryHeader/StoryContent/FounderInfo/FounderInfo.js
@@ -1,17 +1,23 @@
 import React from 'react';
 import './FounderInfo.css'
+import FounderTitle from './FounderTitle/FounderTitle';
 
 const FounderInfo = () => {
     return (
         <div className='founderInfo'>
             <div className="info-bg">
-            <div className="hrBg">
+                <div className="hrBg">
                     <hr className='hrBgTi1' />
                 </div>
                 <h1 className='needHelpFounder'>Simplicity carried to an extreme becomes elegance</h1>
 
             </div>
-            
+            <div className="founderContent">
+                <div className="founderTitleHead">
+                    <FounderTitle/>
+                </div>
+            </div>
+
         </div>
     );
 };
---
+++ src/Page/AboutUs/StoryHeader/StoryContent/FounderInfo/FounderTitle/FounderTitle.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './FounderTitle.css';
+
+const FounderTitle = () => {
+    return (
+        <div className='founderTitle'>
+            <div className='helpHeader'>
+                <h5 className='supportCls'>our story</h5>
+                <h1 className='title'>How it all started</h1>
+                <div className="hr-sec">
+                    <hr className='hrHelp' />
+                </div>
+                <p className='des-founderTitle'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cupiditate ipsum dolorem error, beatae unde exercitationem? Doloremque, excepturi fugiat quisquam perferendis amet, enim magni molestias dolore et odio tempore eos voluptatem quam nihil, provident asperiores earum dolores nemo aspernatur explicabo suscipit?.</p>
+            </div>
+
+        </div>
+    );
+};
+
+export default FounderTitle;
