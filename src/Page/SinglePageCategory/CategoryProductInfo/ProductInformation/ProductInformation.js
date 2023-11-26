672513f288623826aa0729749d660c901e5b291d
TamannaAfroz2666 <afroztamanna66@gmail.com>
2023-11-26 11:41:33 +0600

    product list head title add

--- src/Page/SinglePageCategory/CategoryProductInfo/ProductInformation/ProductInformation.js
+++ src/Page/SinglePageCategory/CategoryProductInfo/ProductInformation/ProductInformation.js
@@ -7,7 +7,7 @@ const ProductInformation = () => {
     return (
         <div className='ProductInformation'>
             <div className="product_header">
-                <ul className='pro-head-ul'>
+                <ul className='pro-head-ul22'>
                     <li>
                         <Link to='/' className='pro-head-link'>Home</Link>
                     </li>
@@ -24,6 +24,12 @@ const ProductInformation = () => {
                         Product Name 10
                     </li>
                 </ul>
+                <div className="product-list">
+                    <Link to='product-category/bedroom' className='pro-list-link'>Bedroom,</Link>
+                    <Link to='product-category/decor' className='pro-list-link'>Decor,</Link>
+                    <Link to='product-category/office' className='pro-list-link'>office</Link>
+                </div>
+                
             </div>
         </div>
     );
