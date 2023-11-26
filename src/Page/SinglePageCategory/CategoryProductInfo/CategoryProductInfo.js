1927389d2a42737f28f00458dd0ca16db3586101
TamannaAfroz2666 <afroztamanna66@gmail.com>
2023-11-26 10:40:15 +0600

    sub image done

--- src/Page/SinglePageCategory/CategoryProductInfo/CategoryProductInfo.js
+++ src/Page/SinglePageCategory/CategoryProductInfo/CategoryProductInfo.js
@@ -4,6 +4,9 @@ import im1 from '../../../assets/Image/category/office.jpg';
 // import { Link } from 'react-router-dom';
 import Button from '@mui/material/Button';
 import { IoSearchSharp } from "react-icons/io5";
+import im2 from '../../../assets/Image/category/product-09-c.jpg'
+import { FaArrowsUpDown } from "react-icons/fa6";
+import { Link } from 'react-router-dom';
 
 const CategoryProductInfo = () => {
     return (
@@ -23,6 +26,25 @@ const CategoryProductInfo = () => {
                     </div>
                     <div className="imgCateSec">
                         <img src={im1} alt="" />
+                    </div>
+                    <div className="colorImgSection">
+                        <Link to="product/upload/01">
+                        <div className="imageOneSec">
+                            <div className="upDownArrow">
+                                <Button > <span className='linkViewBtn'><FaArrowsUpDown className='iconDeEye' size={20} color='gray' /> </span>
+                                </Button>
+                            </div>
+                            <div className="img11">
+                                <img src={im2} alt="" />
+                            </div>
+                        </div>
+                        </Link>
+                        <div className="imageTwo">
+                            <img src={im2} alt="" />
+
+                        </div>
+
+
                     </div>
 
 
