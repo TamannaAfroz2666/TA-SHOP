38c09bf9512c45dcc5203b9aa9c2153ce4a46570
TamannaAfroz2666 <afroztamanna66@gmail.com>
2023-11-27 01:00:08 +0600

    title add

--- src/Page/SinglePageCategory/CategoryDescription/CategoryDescription.js
+++ src/Page/SinglePageCategory/CategoryDescription/CategoryDescription.js
@@ -9,11 +9,25 @@ const CategoryDescription = () => {
                     <h2 className='title'>Description</h2>
                 </div>
                 <div className="titlehr">
-                    <hr className='categoryDesHr'/>
+                    <hr className='categoryDesHr22' />
+
                 </div>
             </div>
+            <div className="subSectionCategoryInfo">
+                <div className="infoBasic">
+                    <h1 className='subTitleCategory'>A few words about the product</h1>
+                    <hr className='titleSecHr' />
 
-            
+                </div>
+                <div className="infoBasic2">
+                    <h3 className='basicTitle'>Aenean pretium, sem vitae gravida tincidunt, justo diam tempus mauris.</h3>
+                    <p className='basicTitle2'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin vestibulum erat leo, id pulvinar lorem maximus sit amet. Quisque mauris sem, sagittis sed blandit eu, varius a tortor. Quisque aliquam ligula sed quam pulvinar, vitae ullamcorper metus imperdiet.</p>
+                </div>
+
+
+            </div>
+
+
         </div>
     );
 };
