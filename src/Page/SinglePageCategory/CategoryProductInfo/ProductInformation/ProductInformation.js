2fdca6bdbf15801c2e6a288906aa009024b7ff6d
TamannaAfroz2666 <afroztamanna66@gmail.com>
2023-11-26 12:38:56 +0600

    add to card button add

--- src/Page/SinglePageCategory/CategoryProductInfo/ProductInformation/ProductInformation.js
+++ src/Page/SinglePageCategory/CategoryProductInfo/ProductInformation/ProductInformation.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import './ProductInformation.css'
 import { Link } from 'react-router-dom';
+import { MdOutlineFavoriteBorder } from "react-icons/md";
+import { AiOutlinePlus, AiOutlineMinus } from "react-icons/ai";
 
 
 const ProductInformation = () => {
@@ -37,6 +39,41 @@ const ProductInformation = () => {
                     </p>
                     <p className='pro-des-cate'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin vestibulum erat leo, id pulvinar lorem maximus sit amet. Quisque mauris sem, sagittis sed blandit eu, varius a tortor. Quisque aliquam ligula sed quam pulvinar, vitae ullamcorper metus imperdiet. Vivamus id justo dui.</p>
                     <p className='pro-des-cate'>Donec faucibus gravida vulputate. Sed quis porttitor libero. Aenean posuere imperdiet elit, in aliquet lorem vulputate sed. Integer et lacinia tellus.</p>
+
+                    {/* color section  */}
+
+                    <hr />
+                    <div className="btnAddSection">
+                        <div className="increaseDeSection">
+                            <div className="cart-sec">
+                                <div className="inputValue">
+                                    <div className="incrDecr-infoSec">
+                                        <div className="decr-sec22">
+                                            <button className='decreaseBtn22'>
+                                                <AiOutlineMinus size={20} />
+                                            </button>
+                                        </div>
+                                        <div className="value-pro-show">
+                                            <span className='showOne1'>1</span>
+                                        </div>
+                                        <div className="incr-sec22">
+                                            <button className='increaseBtn22'>
+                                                <AiOutlinePlus size={20} />
+                                            </button>
+                                        </div>
+
+                                    </div>
+
+                                </div>
+                                <div className="cardBtnInformation">
+                                    <button className='add-card-cateBtn'>add to card</button>
+                                </div>
+
+                            </div>
+
+                        </div>
+                    </div>
+                    <hr />
 
                 </div>
 
