f3abc0d4ef67950e2c61b4dcb68f54448c898ed0
TamannaAfroz2666 <afroztamanna66@gmail.com>
2023-11-28 08:32:45 +0600

    add

--- src/Page/SinglePageCategory/CategoryDescription/CategoryDescription.js
+++ src/Page/SinglePageCategory/CategoryDescription/CategoryDescription.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import './CategoryDescription.css';
 import img1 from '../../../assets/Image/category/img11.webp';
+import img2 from '../../../assets/Image/category/1700300404186.jpg';
 import { FaCheck } from "react-icons/fa";
+
 
 const CategoryDescription = () => {
     return (
@@ -12,7 +14,6 @@ const CategoryDescription = () => {
                 </div>
                 <div className="titlehr">
                     <hr className='categoryDesHr22' />
-
                 </div>
             </div>
             <div className="subSectionCategoryInfo">
@@ -30,14 +31,31 @@ const CategoryDescription = () => {
                                 <li className='listsFeaCate'><FaCheck /> <span className='listSubSpan'>Lorem ipsum dolor sit amet</span></li>
                             </ul>
                         </div>
-                    </div>
 
+                    </div>
+                    <div className="featureImage">
+                        <img src={img2} alt="" />
+                    </div>
                 </div>
                 <div className="infoBasic2">
                     <h3 className='basicTitle'>Aenean pretium, sem vitae gravida tincidunt, justo diam tempus mauris.</h3>
                     <p className='basicTitle2'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin vestibulum erat leo, id pulvinar lorem maximus sit amet. Quisque mauris sem, sagittis sed blandit eu, varius a tortor. Quisque aliquam ligula sed quam pulvinar, vitae ullamcorper metus imperdiet.</p>
                     <div className="categoryDesImg">
                         <img src={img1} alt="" />
+                    </div>
+                    <div className="featuresCategory2">
+                        <h1 className='title'>Care
+Instructions</h1>
+                        <p className='featuresDesCategory'>Ut at ante diam. Vestibulum tincidunt lacus quis odio iaculis, nec iaculis ipsum hendrerit. Curabitur nec fringilla sem. Nullam at diam et ligula tincidunt luctus. Ut fringilla vitae orci eget suscipit. Etiam ultricies justo ac feugiat dignissim. Suspendisse in ultrices massa.</p>
+                        <div className="ulFeatures2">
+                            <ul>
+                                <li className='listsFeaCate'><FaCheck /> <span className='listSubSpan'>Etiam eu tortor tempor, malesuada</span></li>
+                                <li className='listsFeaCate'> <FaCheck /><span className='listSubSpan'>
+                                    Nunc vitae erat sit amet neque varius consequat</span></li>
+                                <li className='listsFeaCate'><FaCheck /> <span className='listSubSpan'>Lorem ipsum dolor sit amet</span></li>
+                            </ul>
+                        </div>
+
                     </div>
                 </div>
             </div>
